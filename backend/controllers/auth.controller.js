7da046c0310991a7f23c518e3670227614d3d089
ZoDD-Script <blessedchukwu89@gmail.com>
2025-07-15 12:34:28 +0100

    User model file path correcting

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken";
-import User from "../models/User.model.js";
+import User from "../models/users.model.js";
 
 const generateToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "1h" });
